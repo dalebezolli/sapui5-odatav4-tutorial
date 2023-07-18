3aa2a22c19148507988ae9d6b6930744a85cf8d0
Dale Bezolli <deilbezolli@gmail.com>
2023-07-18 09:32:29 +0000

    Add search bar and sort button

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,7 +3,11 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageToast",
 	"sap/m/MessageBox",
-], function (Controller, JSONModel, MessageToast, MessageBox) {
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/FilterType",
+	"sap/ui/model/Sorter",
+], function (Controller, JSONModel, MessageToast, MessageBox, Filter, FilterOperator, FilterType, Sorter) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
@@ -13,7 +17,8 @@ sap.ui.define([
 		 */
 		onInit : function () {
 			var oJSONData = {
-					busy : false
+					busy : false,
+					order: 0
 				},
 				oModel = new JSONModel(oJSONData);
 
@@ -30,6 +35,32 @@ sap.ui.define([
 
 			binding.refresh();
 			MessageToast.show(resourceBundle.getText("refreshSuccessMessage"));
+		},
+		onSearch: function(event) {
+			const userQuery = event.getParameters().query;
+			const filter = new Filter("LastName", FilterOperator.Contains, userQuery);
+
+			this.getView().byId("peopleList").getBinding("items").filter(filter, FilterType.Application);
+		},
+		onSort: function(event) {
+			const view = this.getView();
+			const sortStates = [undefined, "asc", "desc"];
+			const sortStateMessageIds = ["sortNone", "sortAscending", "sortDescending"];
+
+			let order = view.getModel("appView").getProperty("/order");
+			order = (order + 1) % sortStates.length;
+			view.getModel("appView").setProperty("/order", order);
+
+			const currentSortState = sortStates[order];
+
+			view.byId("peopleList").getBinding("items").sort(
+				currentSortState && 
+				new Sorter("LastName", currentSortState === "desc")
+			);
+
+			const resourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			MessageToast.show(resourceBundle.getText(sortStateMessageIds[order]));
+
 		}
 	});
 });
