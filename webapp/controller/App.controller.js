41dcbfba4435ab24fff65246a7e579dd15fd8db1
Dale Bezolli <deilbezolli@gmail.com>
2023-07-18 08:00:35 +0000

    Add refresh button

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox",
+], function (Controller, JSONModel, MessageToast, MessageBox) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
@@ -16,6 +18,18 @@ sap.ui.define([
 				oModel = new JSONModel(oJSONData);
 
 			this.getView().setModel(oModel, "appView");
+		},
+		onRefresh: function() {
+			const resourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+
+			const binding = this.byId("peopleList").getBinding("items");
+			if(binding.hasPendingChanges()) {
+				MessageBox.error(resourceBundle.getText("refreshNotPossibleMessage"));
+				return;
+			}
+
+			binding.refresh();
+			MessageToast.show(resourceBundle.getText("refreshSuccessMessage"));
 		}
 	});
 });
